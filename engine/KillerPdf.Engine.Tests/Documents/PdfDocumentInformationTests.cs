ec2a84a4fb3257c70eef86ff50f244ce2b5f72e7
Steve <steve@thekiller.net>
2026-09-18 23:48:24 -0700

    v1.9.0: read a null /Trapped document information entry

--- engine/KillerPdf.Engine.Tests/Documents/PdfDocumentInformationTests.cs
+++ engine/KillerPdf.Engine.Tests/Documents/PdfDocumentInformationTests.cs
@@ -8,6 +8,37 @@ namespace KillerPdf.Engine.Tests.Documents;
 public sealed class PdfDocumentInformationTests
 {
     [Fact]
+    public void ReadTreatsANullTrappedEntryAsAbsent()
+    {
+        string[] objects =
+        [
+            "<< /Type /Catalog /Pages 2 0 R >>",
+            "<< /Type /Pages /Kids [3 0 R] /Count 1 >>",
+            "<< /Type /Page /Parent 2 0 R /MediaBox [0 0 10 10] >>",
+            "<< /Title (Report) /Trapped null >>"
+        ];
+        var pdf = new System.Text.StringBuilder("%PDF-1.7\n");
+        var offsets = new List<int>();
+        for (int index = 0; index < objects.Length; index++)
+        {
+            offsets.Add(System.Text.Encoding.Latin1.GetByteCount(pdf.ToString()));
+            pdf.Append($"{index + 1} 0 obj\n{objects[index]}\nendobj\n");
+        }
+        int xref = System.Text.Encoding.Latin1.GetByteCount(pdf.ToString());
+        pdf.Append($"xref\n0 {objects.Length + 1}\n0000000000 65535 f \n");
+        foreach (int offset in offsets) pdf.Append($"{offset:0000000000} 00000 n \n");
+        pdf.Append($"trailer << /Size {objects.Length + 1} /Root 1 0 R /Info 4 0 R >>\n"
+            + $"startxref\n{xref}\n%%EOF\n");
+        PdfDocument document = PdfDocument.Open(
+            System.Text.Encoding.Latin1.GetBytes(pdf.ToString()));
+
+        PdfDocumentInformation information = PdfDocumentInformation.Read(document);
+
+        Assert.Null(information.Trapped);
+        Assert.Equal("Report", information.Title);
+    }
+
+    [Fact]
     public void Read_ReturnsMetadataVersionAndPageCount()
     {
         byte[] bytes = new PdfDocumentBuilder(PdfVersion.Pdf20)
